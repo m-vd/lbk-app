7892a3e8f11bd4d3a6f0dc6571b8d7270239fb6d
m-vd <mikvadi.9@gmail.com>
2018-11-22 15:41:52 +0700

    testing sso

--- app.js
+++ app.js
@@ -11,11 +11,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/views"));
 app.set("view engine", "ejs");
 
-mongoose.connect("mongodb://localhost/lbk");
+//mongoose.connect("mongodb://localhost/lbk");
 
 app.get("/", function (req, res) {
+    console.log(req.headers.host);
+    console.log(req.originalUrl);
     res.render("index");
-})
+});
+
+app.get("/login", function(req, res){
+    if (req.query.ticket) {
+        var service = "service=" + encodeURIComponent(req.headers.host + req.originalUrl);
+        var ticket = "ticket=" + req.query.ticket;
+        res.redirect("https://login.itb.ac.id/cas/serviceValidate?" + service + "&" + ticket);
+    } else {
+        var itbloginuri = "https://login.itb.ac.id/cas/"
+        res.redirect(encodeURIComponent(itbloginuri + "login?service=" + req.headers.host + req.originalUrl))
+    }
+});
+
 
 app.get("/services", function (req, res) {
     Psychologist.find({}, function(err, allPyschologists){
